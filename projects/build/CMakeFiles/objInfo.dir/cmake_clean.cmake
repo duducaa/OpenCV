321e52b9bc3f3d0a24caac1a45fa91d6364f6192
duducaa <110697369+duducaa@users.noreply.github.com>
2023-01-20 05:53:20 -0300

    update

---
+++ projects/build/CMakeFiles/objInfo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objInfo.dir/src/objInfo.cpp.o"
+  "CMakeFiles/objInfo.dir/src/objInfo.cpp.o.d"
+  "objInfo"
+  "objInfo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objInfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
